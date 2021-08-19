52fc5397c92c8c3335201635eb91ede19b98a9a2
PoojaPatil3456 <poojapatil2402@gmail.com>
2021-08-19 21:50:04 +0530

    Req 1:To print fizz and buzz when number divisible by 3 and 5 respectively

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,13 @@
 public class FizzBuzz {
+    int number;
+    public FizzBuzz(int number){
+        this.number=number;
+    }
+    public String fizzBuzz(){
+        if(number%3==0)
+            return "Fizz";
+        if(number%5==0)
+            return "Buzz";
+        return null;
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,3 +1,16 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void toPrintFizzWhenNumberDivisibleByThree(){
+        FizzBuzz fizzbuzz=new FizzBuzz(9);
+        assertEquals("Fizz",fizzbuzz.fizzBuzz());
+    }
+    @Test
+    void toPrintBuzzWhenNumberDivisibleByFive(){
+        FizzBuzz fizzbuzz=new FizzBuzz(10);
+        assertEquals("Buzz",fizzbuzz.fizzBuzz());
+    }
 
 }
